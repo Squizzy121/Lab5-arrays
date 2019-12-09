d85d0972b4b53ed44f50bc6520796509e21b4edb
Squizzy121 <danil-b@ukr.net>
2019-12-09 18:08:49 +0200

    Улучшена и готова к сдаче

--- Program.cs
+++ Program.cs
@@ -7,58 +7,70 @@ namespace Zadacha5._1
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Введіть в строку число, яким методом ви хочете вводити числа в массив:\nРандомними числами(Введіть 1)\nВручну(Введіть 2)");
+            int choice = int.Parse(Console.ReadLine());
             Console.WriteLine("Введіть кількість елементів массиву");
             int n = int.Parse(Console.ReadLine());
-            CreateNewArray(n);
+            int[] array;
+            if (choice == 1)
+            {
+                array = FillingNewArrayWithRandomNumbers(n);
+            }
+            else
+            {
+                array = FillingNewArrayWithNumbersFromKeyboard(n);
+            }
+            PrintArray(array,n);
+            double multiplication= MultiplyNumbersInArray(array, n);
+            Console.WriteLine("\nдобуток елементів, які стоять перед останім входженням максимального числа дорівнює: {0}",multiplication);
             Console.ReadKey();
         }
-        static void CreateNewArray(int n)
+        static int[] FillingNewArrayWithNumbersFromKeyboard(int n)
         {
-            int max = int.MinValue;
-            int IndexOfMax=-1;
             int[] array = new int[n];
-            /*for(int i=0;i<n;i++)
-             * {
-             * int num=int.Parse(Console.ReadLine());
-             * array[i]=num;
-             * if (num>=max)
-             *   {
-             *   max=num;
-             *   IndexOfMax=i;
-             *   }
-             * }
-             */
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                array[i] = num;
+            }
+            return array;
+        }
+        static int[] FillingNewArrayWithRandomNumbers(int n)
+        {
+            int[] array = new int[n];
             Random rand = new Random();
             for (int i = 0; i < n; i++)
             {
-                array[i] = rand.Next(-101,101);
-                if (array[i] >= max)
-                {
-                    max = array[i];
-                    IndexOfMax = i;
-                }
+                array[i] = rand.Next(-101, 101);
             }
-            PrintArray(array, n);
-            MultiplyNumbersInArray(array, IndexOfMax);
-
+            return array;
         }
         static void PrintArray(int[] array, int n)
         {
-            Console.WriteLine("Перелік елементів массиву");
+            Console.WriteLine("Перелік елементів массиву:");
             for (int k = 0; k < n; k++)
             {
                 Console.Write("{0} ", array[k]);
             }
         }
-        static void MultiplyNumbersInArray(int[] array, int IndexOfMax)
+        static double MultiplyNumbersInArray(int[] array, int n)
         {
-            double multiply = 1;
+            int max = int.MinValue;
+            int IndexOfMax = -1;
+            double multiplication = 1;
+            for (int q = 0; q < n; q++)
+            {
+                if (array[q] >= max)
+                {
+                    max = array[q];
+                    IndexOfMax = q;
+                }
+            }
             for (int q = 0; q < IndexOfMax; q++)
             {
-                    multiply *= array[q];
+                multiplication *= array[q];
             }
-            Console.WriteLine("\nДобуток чисел перед останнім входженням максимального числа дорівнює {0}", multiply);
+            return multiplication;
         }
-
     }
 }
